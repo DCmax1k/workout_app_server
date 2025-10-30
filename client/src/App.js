4465d085091ba19c071fd4672f77e5488e498997
Dylan Caldwell <dylan.caldwell35@gmail.com>
2025-10-29 23:29:47 -0400

    build

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div>
+          Coming soon! Check out progress at the 
+        </div>
+        <a target='_blank' href='https://github.com/DCmax1k/workout_app'>github</a>
+      
+    </div>
+  );
+}
+
+export default App;
