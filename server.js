d83e623210e54b1e3cd571e70798406034145d0b
Dylan Caldwell <dylan.caldwell35@gmail.com>
2025-12-23 21:07:02 -0500

    new version

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 
 // VERSION
-const VERSION = "1.0.8";
+const VERSION = "1.0.9";
 
 // Middlewares
 app.use(express.urlencoded({ extended: true }));
